a2cf6de331f0e2d26a459cd812bfdcf2df76250a
matsushinDB11 <matsushin35@gmail.com>
2021-09-22 02:01:34 +0900

    App.js の大半を SqlLoad.jsx に移植

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
+import SqlLoad from "./SqlLoad";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
+    <SqlLoad />
     <App />
   </React.StrictMode>,
   rootElement
