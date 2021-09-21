e6e254d5856fea7bc64c07e4521f71d6fba16b24
matsushinDB11 <matsushin35@gmail.com>
2021-09-22 02:34:23 +0900

    done prettier

--- src/SqlLoad.jsx
+++ src/SqlLoad.jsx
@@ -6,21 +6,21 @@ export default function SqlLoad() {
   const [dbState, setDb] = useState(null);
   const [error, setError] = useState(null);
   useEffect(() => {
-      Promise.all([initSqlJs(), fetch("./database1.sqlite3")])
-        .then(async (res) => {
-          const SQLite = res[0],
-            dbStorage = res[1];
-          const db = new SQLite.Database(
-            new Uint8Array(await dbStorage.arrayBuffer())
-          );
-          // me.setState({db: db});
-          setDb(db);
-        })
-        .catch((err) => {
-          // me.setState({err});
-          setError(err);
-        });
-  },[]);
+    Promise.all([initSqlJs(), fetch("./database1.sqlite3")])
+      .then(async (res) => {
+        const SQLite = res[0],
+          dbStorage = res[1];
+        const db = new SQLite.Database(
+          new Uint8Array(await dbStorage.arrayBuffer())
+        );
+        // me.setState({db: db});
+        setDb(db);
+      })
+      .catch((err) => {
+        // me.setState({err});
+        setError(err);
+      });
+  }, []);
 
   // return <p>gegege</p>;
   if (error) return <pre>{error.toString()}</pre>;
@@ -33,43 +33,43 @@ export default function SqlLoad() {
  * @param {{db: import("sql.js").Database}} props
  */
 function SQLRepl({ db }) {
-    const [error, setError] = useState(null);
-    const [results, setResults] = useState([]);
+  const [error, setError] = useState(null);
+  const [results, setResults] = useState([]);
 
-    function exec(sql) {
-        try {
-            // The sql is executed synchronously on the UI thread.
-            // You may want to use a web worker here instead
-            setResults(db.exec(sql)); // an array of objects is returned
-            setError(null);
-        } catch (err) {
-            // exec throws an error when the SQL statement is invalid
-            setError(err);
-            setResults([]);
-        }
+  function exec(sql) {
+    try {
+      // The sql is executed synchronously on the UI thread.
+      // You may want to use a web worker here instead
+      setResults(db.exec(sql)); // an array of objects is returned
+      setError(null);
+    } catch (err) {
+      // exec throws an error when the SQL statement is invalid
+      setError(err);
+      setResults([]);
     }
+  }
 
-    return (
-        <div className="App">
-            <h1>React SQL interpreter</h1>
+  return (
+    <div className="App">
+      <h1>React SQL interpreter</h1>
 
-            <textarea
-                onChange={(e) => exec(e.target.value)}
-                placeholder="Enter some SQL. No inspiration ? Try “select sqlite_version()”"
-            ></textarea>
+      <textarea
+        onChange={(e) => exec(e.target.value)}
+        placeholder="Enter some SQL. No inspiration ? Try “select sqlite_version()”"
+      ></textarea>
 
-            <pre className="error">{(error || "").toString()}</pre>
+      <pre className="error">{(error || "").toString()}</pre>
 
-            <pre>
+      <pre>
         {
-            // results contains one object per select statement in the query
-            results.map(({ columns, values }, i) => (
-                <ResultsTable key={i} columns={columns} values={values} />
-            ))
+          // results contains one object per select statement in the query
+          results.map(({ columns, values }, i) => (
+            <ResultsTable key={i} columns={columns} values={values} />
+          ))
         }
       </pre>
-        </div>
-    );
+    </div>
+  );
 }
 
 /**
@@ -77,29 +77,28 @@ function SQLRepl({ db }) {
  * @param {import("sql.js").QueryExecResult} props
  */
 function ResultsTable({ columns, values }) {
-    return (
-        <table>
-            <thead>
-            <tr>
-                {columns.map((columnName, i) => (
-                    <td key={i}>{columnName}</td>
-                ))}
+  return (
+    <table>
+      <thead>
+        <tr>
+          {columns.map((columnName, i) => (
+            <td key={i}>{columnName}</td>
+          ))}
+        </tr>
+      </thead>
+
+      <tbody>
+        {
+          // values is an array of arrays representing the results of the query
+          values.map((row, i) => (
+            <tr key={i}>
+              {row.map((value, i) => (
+                <td key={i}>{value}</td>
+              ))}
             </tr>
-            </thead>
-
-            <tbody>
-            {
-                // values is an array of arrays representing the results of the query
-                values.map((row, i) => (
-                    <tr key={i}>
-                        {row.map((value, i) => (
-                            <td key={i}>{value}</td>
-                        ))}
-                    </tr>
-                ))
-            }
-            </tbody>
-        </table>
-    );
+          ))
+        }
+      </tbody>
+    </table>
+  );
 }
-
